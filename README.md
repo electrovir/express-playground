ed1df68a4af8305f9c4bbc98edc6a4b19cad40d1
electrovir <electrovir@users.noreply.github.com>
2023-08-14 19:28:37 +0000

    test out concurrency

---
+++ README.md
@@ -0,0 +1,6 @@
+# express-playground
+
+## Resources
+
+-   https://www.digitalocean.com/community/tutorials/how-to-use-multithreading-in-node-js
+-   http://npmjs.com/express
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import {resolve} from 'path';
+import {Worker} from 'worker_threads';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/non-blocking/', (req, res) => {
+    res.status(200).send('This page is non-blocking');
+});
+
+function calculateCount() {
+    return new Promise((resolve, reject) => {
+        let counter = 0;
+        for (let i = 0; i < 20_000_000_000; i++) {
+            counter++;
+        }
+        resolve(counter);
+    });
+}
+
+// // blocks all other requests
+// app.get('/blocking', async (req, res) => {
+//     const counter = await calculateCount();
+//     res.status(200).send(`result is ${counter}`);
+// });
+
+// // blocks all other requests
+// app.get('/blocking', async (req, res) => {
+//     calculateCount().then((counter) => {
+//         res.status(200).send(`result is ${counter}`);
+//     });
+// });
+
+// // still blocks all other requests
+// app.get('/blocking', async (req, res) => {
+//     const counter = calculateCount();
+//     res.status(200).send(`result is not finished`);
+// });
+
+// works
+const workerPath = resolve(__dirname, './worker.ts');
+app.get('/blocking', async (req, res) => {
+    const worker = new Worker(workerPath);
+    worker.on('message', (data) => {
+        res.status(200).send(`result is ${data}`);
+    });
+    worker.on('error', (msg) => {
+        res.status(404).send(`An error occurred: ${msg}`);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
